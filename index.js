db801329971c1ffda70dd772ab2ceac893a762ca
Rgamer43 <rddellosso4@gmail.com>
2021-09-02 13:50:17 -0400

    Added an easteregg

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello World")
+let input = prompt("There is an easteregg on this website. What do you have to say about this?");
+console.log(input);
+
+if(input == "easteregg"){
+	alert("You have discovered an easteregg!");
+}

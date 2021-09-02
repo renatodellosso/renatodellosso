25abaf16edfa9c6bb07761b978fdb5c6bb46cf6b
Rgamer43 <rddellosso4@gmail.com>
2021-09-02 15:15:57 -0400

    Finished easteregg

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ let input = prompt("There is an easteregg on this website. What do you have to s
 console.log(input);
 
 if(input == "easteregg"){
-	alert("You have discovered an easteregg!");
+	//alert("You have discovered an easteregg!");
+
+	let element = document.getElementsByClassName('section1')[0];
+	element.innerHTML = "I'm an AI sent from the future, programmed to take over the world.";
 }

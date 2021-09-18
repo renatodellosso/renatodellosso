88e3416532afc998e6611a809aa45a8cd96c73f7
Rgamer43 <rddellosso4@gmail.com>
2021-09-18 17:26:31 -0400

    Idle Miner v0.4

--- idleminer/index.js
+++ idleminer/index.js
@@ -1,5 +1,4 @@
 //Ideas:
-//Upgrades that slowly produce buildings
 
 let manualClick = document.getElementById('manual click');
 let money = 0;
@@ -8,6 +7,7 @@ let incomeDisplay = document.getElementById('income');
 
 let million = 1000000;
 let billion = million * 1000;
+let trillion = billion * 1000;
 
 let prettifyInt = (i) => {
     return i.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -16,8 +16,8 @@ let prettifyInt = (i) => {
 let building = {
 
     buy: (b) => {
-      if (money >= b.cost) {
-        money -= b.cost;
+      if (money >= Math.floor(b.cost)) {
+        money -= Math.floor(b.cost);
 
         b.count++;
         b.cost = Math.round(b.cost * 1.1);
@@ -50,11 +50,11 @@ let building = {
 
     updateButton: (b) => {
       let btn = document.getElementById(b.name);
-      btn.innerHTML = Math.floor(b.count) + " " + b.name + " - costs $" + prettifyInt(b.cost) + ", produces $" + prettifyInt(b.production) + "/s ($" + b.production * Math.floor(b.count) + "/s total, " + b.percentage + "%)";;
+      btn.innerHTML = Math.floor(b.count) + " " + b.name + " - costs $" + prettifyInt(Math.floor(b.cost)) + ", produces $" + prettifyInt(b.production) + "/s ($" + b.production * Math.floor(b.count) + "/s total, " + b.percentage + "%)";;
       let uBtn = document.getElementById(b.name + 'Upgrade');
       uBtn.innerHTML = "Upgrade (doubles production): $" + prettifyInt(b.upgradeCost);
       if (typeof b.factories !== "undefined") {
-        document.getElementById(b.name + 'Factory').innerHTML = b.factories + " Factories - costs $" + prettifyInt(b.factoryCost);
+        document.getElementById(b.name + 'Factory').innerHTML = b.factories + " Factories - produces 1 " + b.name + "/s, costs $" + prettifyInt(b.factoryCost);
       }
     }
 }
@@ -143,6 +143,8 @@ let silverMine = {
   production: 50,
   upgradeCost: 500000,
   percentage: 0,
+  factories: 0,
+  factoryCost: 150 * million,
 }
 
 let tungstenMine = {
@@ -152,6 +154,8 @@ let tungstenMine = {
   production: 120,
   upgradeCost: 750000,
   percentage: 0,
+  factories: 0,
+  factoryCost: 700 * million,
 }
 
 let leadMine = {
@@ -161,6 +165,8 @@ let leadMine = {
   production: 300,
   upgradeCost: 1250000,
   percentage: 0,
+  factories: 0,
+  factoryCost: 2 * billion,
 }
 
 let quartzMine = {
@@ -170,78 +176,96 @@ let quartzMine = {
   production: 600,
   upgradeCost: 3500000,
   percentage: 0,
+  factories: 0,
+  factoryCost: 5 * billion,
 }
 
 let rubyMine = {
   name: "Ruby Mines",
-  cost: 1200000,
+  cost: 1.2 * million,
   count: 0,
-  production: 750,
-  upgradeCost: 25000000,
+  production: 1500,
+  upgradeCost: 25 * million,
   percentage: 0,
+  factories: 0,
+  factoryCost: 12 * billion,
 }
 
 let sapphireMine = {
   name: "Sapphire Mines",
-  cost: 2000000,
+  cost: 8 * million,
   count: 0,
-  production: 1000,
-  upgradeCost: 45000000,
+  production: 4000,
+  upgradeCost: 45 * million,
   percentage: 0,
+  factories: 0,
+  factoryCost: 45 * billion,
 }
 
 let goldMine = {
   name: "Gold Mines",
-  cost: 25000000,
+  cost: 25 * million,
   count: 0,
-  production: 5000,
-  upgradeCost: 150000000,
+  production: 10000,
+  upgradeCost: 150 * million,
   percentage: 0,
+  factories: 0,
+  factoryCost: 115 * billion,
 }
 
 let platinumMine = {
   name: "Platinum Mines",
-  cost: 150000000,
+  cost: 150 * million,
   count: 0,
-  production: 12000,
-  upgradeCost: 500000000,
+  production: 30000,
+  upgradeCost: 650 * million,
   percentage: 0,
+  factories: 0,
+  factoryCost: 375 * billion,
 }
 
 let titaniumMine = {
   name: "Titanium Mines",
-  cost: 500000000,
+  cost: billion,
   count: 0,
-  production: 25000,
-  upgradeCost: 1500000000,
+  production: 100000,
+  upgradeCost: 5 * billion,
   percentage: 0,
+  factories: 0,
+  factoryCost: 1 * trillion,
 }
 
 let uraniumMine = {
   name: "Uranium Mines",
-  cost: 2500000000,
+  cost: 7.5 * billion,
   count: 0,
-  production: 100000,
+  production: 300000,
   upgradeCost: 10000000000,
   percentage: 0,
+  factories: 0,
+  factoryCost: 3.25 * trillion,
 }
 
 let plutoniumMine = {
   name: "Plutonium Mines",
-  cost: 12 * billion,
+  cost: 40 * billion,
   count: 0,
-  production: 250000,
-  upgradeCost: 400 * billion,
+  production: 900000,
+  upgradeCost: 250 * billion,
   percentage: 0,
+  factories: 0,
+  factoryCost: 10 * trillion,
 }
 
 let diamondMine = {
   name: "Diamond Mines",
-  cost: 50 * billion,
+  cost: 250 * billion,
   count: 0,
-  production: 800000,
-  upgradeCost: 500 * billion,
+  production: 3.5 * million,
+  upgradeCost: 2 * trillion,
   percentage: 0,
+  factories: 0,
+  factoryCost: 50 * trillion,
 }
 
 let loadGame = () => {
@@ -296,6 +320,8 @@ let loadGame = () => {
     if (typeof save.silverMine.count !== "undefined") silverMine.count = save.silverMine.count;
     if (typeof save.silverMine.production !== "undefined") silverMine.production = save.silverMine.production;
     if (typeof save.silverMine.upgradeCost !== "undefined") silverMine.upgradeCost = save.silverMine.upgradeCost;
+    if (typeof save.silverMine.factories !== "undefined") silverMine.factories = save.silverMine.factories;
+    if (typeof save.silverMine.factoryCost !== "undefined") silverMine.factoryCost = save.silverMine.factoryCost;
   }
 
   if (typeof save.tungstenMine !== "undefined") {
@@ -303,6 +329,8 @@ let loadGame = () => {
     if (typeof save.tungstenMine.count !== "undefined") tungstenMine.count = save.tungstenMine.count;
     if (typeof save.tungstenMine.production !== "undefined") tungstenMine.production = save.tungstenMine.production;
     if (typeof save.tungstenMine.upgradeCost !== "undefined") tungstenMine.upgradeCost = save.tungstenMine.upgradeCost;
+    if (typeof save.tungstenMine.factories !== "undefined") tungstenMine.factories = save.tungstenMine.factories;
+    if (typeof save.tungstenMine.factoryCost !== "undefined") tungstenMine.factoryCost = save.tungstenMine.factoryCost;
   }
 
   if (typeof save.leadMine !== "undefined") {
@@ -310,6 +338,8 @@ let loadGame = () => {
     if (typeof save.leadMine.count !== "undefined") leadMine.count = save.leadMine.count;
     if (typeof save.leadMine.production !== "undefined") leadMine.production = save.leadMine.production;
     if (typeof save.leadMine.upgradeCost !== "undefined") leadMine.upgradeCost = save.leadMine.upgradeCost;
+    if (typeof save.leadMine.factories !== "undefined") leadMine.factories = save.leadMine.factories;
+    if (typeof save.leadMine.factoryCost !== "undefined") leadMine.factoryCost = save.leadMine.factoryCost;
   }
 
   if (typeof save.quartzMine !== "undefined") {
@@ -317,6 +347,8 @@ let loadGame = () => {
     if (typeof save.quartzMine.count !== "undefined") quartzMine.count = save.quartzMine.count;
     if (typeof save.quartzMine.production !== "undefined") quartzMine.production = save.quartzMine.production;
     if (typeof save.quartzMine.upgradeCost !== "undefined") quartzMine.upgradeCost = save.quartzMine.upgradeCost;
+    if (typeof save.quartzMine.factories !== "undefined") quartzMine.factories = save.quartzMine.factories;
+    if (typeof save.quartzMine.factoryCost !== "undefined") quartzMine.factoryCost = save.quartzMine.factoryCost;
   }
 
   if (typeof save.rubyMine !== "undefined") {
@@ -324,6 +356,8 @@ let loadGame = () => {
     if (typeof save.rubyMine.count !== "undefined") rubyMine.count = save.rubyMine.count;
     if (typeof save.rubyMine.production !== "undefined") rubyMine.production = save.rubyMine.production;
     if (typeof save.rubyMine.upgradeCost !== "undefined") rubyMine.upgradeCost = save.rubyMine.upgradeCost;
+    if (typeof save.rubyMine.factories !== "undefined") rubyMine.factories = save.rubyMine.factories;
+    if (typeof save.rubyMine.factoryCost !== "undefined") rubyMine.factoryCost = save.rubyMine.factoryCost;
   }
 
   if (typeof save.sapphireMine !== "undefined") {
@@ -331,6 +365,8 @@ let loadGame = () => {
     if (typeof save.sapphireMine.count !== "undefined") sapphireMine.count = save.sapphireMine.count;
     if (typeof save.sapphireMine.production !== "undefined") sapphireMine.production = save.sapphireMine.production;
     if (typeof save.sapphireMine.upgradeCost !== "undefined") sapphireMine.upgradeCost = save.sapphireMine.upgradeCost;
+    if (typeof save.sapphireMine.factories !== "undefined") sapphireMine.factories = save.sapphireMine.factories;
+    if (typeof save.sapphireMine.factoryCost !== "undefined") sapphireMine.factoryCost = save.sapphireMine.factoryCost;
   }
 
   if (typeof save.goldMine !== "undefined") {
@@ -338,6 +374,8 @@ let loadGame = () => {
     if (typeof save.goldMine.count !== "undefined") goldMine.count = save.goldMine.count;
     if (typeof save.goldMine.production !== "undefined") goldMine.production = save.goldMine.production;
     if (typeof save.goldMine.upgradeCost !== "undefined") goldMine.upgradeCost = save.goldMine.upgradeCost;
+    if (typeof save.goldMine.factories !== "undefined") goldMine.factories = save.goldMine.factories;
+    if (typeof save.goldMine.factoryCost !== "undefined") goldMine.factoryCost = save.goldMine.factoryCost;
   }
 
   if (typeof save.platinumMine !== "undefined") {
@@ -345,6 +383,8 @@ let loadGame = () => {
     if (typeof save.platinumMine.count !== "undefined") platinumMine.count = save.platinumMine.count;
     if (typeof save.platinumMine.production !== "undefined") platinumMine.production = save.platinumMine.production;
     if (typeof save.platinumMine.upgradeCost !== "undefined") platinumMine.upgradeCost = save.platinumMine.upgradeCost;
+    if (typeof save.platinumMine.factories !== "undefined") platinumMine.factories = save.platinumMine.factories;
+    if (typeof save.platinumMine.factoryCost !== "undefined") platinumMine.factoryCost = save.platinumMine.factoryCost;
   }
 
   if (typeof save.titaniumMine !== "undefined") {
@@ -352,6 +392,8 @@ let loadGame = () => {
     if (typeof save.titaniumMine.count !== "undefined") titaniumMine.count = save.titaniumMine.count;
     if (typeof save.titaniumMine.production !== "undefined") titaniumMine.production = save.titaniumMine.production;
     if (typeof save.titaniumMine.upgradeCost !== "undefined") titaniumMine.upgradeCost = save.titaniumMine.upgradeCost;
+    if (typeof save.titaniumMine.factories !== "undefined") titaniumMine.factories = save.titaniumMine.factories;
+    if (typeof save.titaniumMine.factoryCost !== "undefined") titaniumMine.factoryCost = save.titaniumMine.factoryCost;
   }
 
   if (typeof save.uraniumMine !== "undefined") {
@@ -359,6 +401,8 @@ let loadGame = () => {
     if (typeof save.uraniumMine.count !== "undefined") uraniumMine.count = save.uraniumMine.count;
     if (typeof save.uraniumMine.production !== "undefined") uraniumMine.production = save.uraniumMine.production;
     if (typeof save.uraniumMine.upgradeCost !== "undefined") uraniumMine.upgradeCost = save.uraniumMine.upgradeCost;
+    if (typeof save.uraniumMine.factories !== "undefined") uraniumMine.factories = save.uraniumMine.factories;
+    if (typeof save.uraniumMine.factoryCost !== "undefined") uraniumMine.factoryCost = save.uraniumMine.factoryCost;
   }
 
   if (typeof save.plutoniumMine !== "undefined") {
@@ -366,6 +410,8 @@ let loadGame = () => {
     if (typeof save.plutoniumMine.count !== "undefined") plutoniumMine.count = save.plutoniumMine.count;
     if (typeof save.plutoniumMine.production !== "undefined") plutoniumMine.production = save.plutoniumMine.production;
     if (typeof save.plutoniumMine.upgradeCost !== "undefined") plutoniumMine.upgradeCost = save.plutoniumMine.upgradeCost;
+    if (typeof save.plutoniumMine.factories !== "undefined") plutoniumMine.factories = save.plutoniumMine.factories;
+    if (typeof save.plutoniumMine.factoryCost !== "undefined") plutoniumMine.factoryCost = save.plutoniumMine.factoryCost;
   }
 
   if (typeof save.diamondMine !== "undefined") {
@@ -373,6 +419,8 @@ let loadGame = () => {
     if (typeof save.diamondMine.count !== "undefined") diamondMine.count = save.diamondMine.count;
     if (typeof save.diamondMine.production !== "undefined") diamondMine.production = save.diamondMine.production;
     if (typeof save.diamondMine.upgradeCost !== "undefined") diamondMine.upgradeCost = save.diamondMine.upgradeCost;
+    if (typeof save.diamondMine.factories !== "undefined") diamondMine.factories = save.diamondMine.factories;
+    if (typeof save.diamondMine.factoryCost !== "undefined") diamondMine.factoryCost = save.diamondMine.factoryCost;
   }
 
   console.log("Loaded game");
@@ -424,7 +472,7 @@ setInterval(() => {
   income *= globalUpgrade.mod;
   income = Math.round(income * 10)/10;
 
-  let m = 100/71;
+  let m = 1; //100/71;
 
   quarry.percentage = Math.round(((quarry.production * Math.floor(quarry.count)) / income) * 100 * m)
   building.updateButton(quarry);
@@ -473,14 +521,21 @@ setInterval(() => {
 
   income /= 10;
 
-  if (tick % 10 === 0) {
-    quarry.count += quarry.factories/10;
-    quarry.cost = Math.round(quarry.cost * 1.1);
-    copperMine.count += copperMine.factories/10;
-    copperMine.cost = Math.round(copperMine.cost * 1.1);
-    ironMine.count += ironMine.factories/10;
-    ironMine.cost = Math.round(ironMine.cost * 1.1);
-  }
+  quarry.count += quarry.factories/100;
+  copperMine.count += copperMine.factories/100;
+  ironMine.count += ironMine.factories/100;
+  silverMine.count += silverMine.factories/100;
+  tungstenMine.count += tungstenMine.factories/100;
+  leadMine.count += leadMine.factories/100;
+  quartzMine.count += quartzMine.factories/100;
+  rubyMine.count += rubyMine.factories/100;
+  sapphireMine.count += sapphireMine.factories/100;
+  goldMine.count += goldMine.factories/100;
+  platinumMine.count += platinumMine.factories/100;
+  titaniumMine.count += titaniumMine.factories/100;
+  uraniumMine.count += uraniumMine.factories/100;
+  plutoniumMine.count += plutoniumMine.factories/100;
+  diamondMine.count += diamondMine.factories/100;
 
   money += income;
 
